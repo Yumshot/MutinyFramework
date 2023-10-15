ae5234c660543d52a9ea040d0816beca74c9f1c7
Yumshot <Clearlyio@protonmail.com>
2023-10-14 22:17:22 -0700

    fix(teleport): fix the Delays on teleporting event

--- buildable/client/events/teleport.ts
+++ buildable/client/events/teleport.ts
@@ -1,23 +1,26 @@
-onNet("MUTINY:CORE:CLIENT:HANDLE_TELEPORT", (coords: { x: number; y: number; z: number; heading: number; }) => {
+import { Delay } from "utils/functions";
+
+onNet(
+  "MUTINY:CORE:CLIENT:HANDLE_TELEPORT",
+  async (coords: { x: number; y: number; z: number; heading: number }) => {
     const ped = PlayerPedId();
     DoScreenFadeOut(10000);
-    FreezeEntityPosition(ped, true)
-    setTimeout(() => {
-        SetEntityInvincible(ped, true);
-        SetEntityCoordsNoOffset(
-          ped,
-          coords.x,
-          coords.y,
-          coords.z,
-          false,
-          false,
-          false
-        );
-        SetEntityHeading(ped, coords.heading);
-        setTimeout(() => {
-            DoScreenFadeIn(10000);
-            SetEntityInvincible(ped, false);
-             FreezeEntityPosition(ped, false);
-        }, 10000);
-    }, 10000);
-})
+    FreezeEntityPosition(ped, true);
+    await Delay(2500);
+    SetEntityInvincible(ped, true);
+    SetEntityCoordsNoOffset(
+      ped,
+      coords.x,
+      coords.y,
+      coords.z,
+      false,
+      false,
+      false
+    );
+    SetEntityHeading(ped, coords.heading);
+    await Delay(2500);
+    DoScreenFadeIn(10000);
+    SetEntityInvincible(ped, false);
+    FreezeEntityPosition(ped, false);
+  }
+);
