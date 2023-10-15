e903885fd01065033a6381dd6171cbe9694d3d74
Yumshot <Clearlyio@protonmail.com>
2023-10-14 22:16:13 -0700

    fix(client): add waits for Requesting Models

--- buildable/client/events/callbacks.ts
+++ buildable/client/events/callbacks.ts
@@ -1,3 +1,5 @@
+import { Delay } from "utils/functions";
+
 const exp = (global as any).exports;
 
 RegisterNuiCallback("create_user_credentials", (data: any, cb: any) => {
@@ -5,18 +7,27 @@ RegisterNuiCallback("create_user_credentials", (data: any, cb: any) => {
     const idents = data.identifiers;
     emitNet("MUTINY:CORE:SERVER:CREATE_FIRST_CHARACTER", [context, idents]);
     CLOSE_NUI_COMPLETELY();
-
+cb("ok");
 });
 
-const baseModels = ["mp_m_freemode_01","mp_f_freemode_01"];
-onNet("MUTINY:CORE:CLIENT:REGISTERED_CHARACTER", (data: any) => {
+const baseModels = ["mp_m_freemode_01", "mp_f_freemode_01"];
+onNet("MUTINY:CORE:CLIENT:REGISTERED_CHARACTER", async (data: any) => {
    SetNuiFocus(false, false);
    const ped = PlayerPedId();
    FreezeEntityPosition(ped, false);
    if (data.sex === "Male") {
-    exp["mutiny_appearance"].setPedModel(baseModels[0]);
+    await Delay(500);
+     RequestModel(baseModels[0]);
+     while (!HasModelLoaded(baseModels[0])) {
+       await Delay(500);
+     }
+    exp["mutiny_appearance"].setPlayerModel(baseModels[0]);
    } else {
-    exp["mutiny_appearance"].setPedModel(baseModels[1]);
+     RequestModel(baseModels[1]);
+     while (!HasModelLoaded(baseModels[1])) {
+       await Delay(500);
+     }
+    exp["mutiny_appearance"].setPlayerModel(baseModels[1]);
    }
    const config = {
     ped: false,
@@ -41,9 +52,11 @@ RegisterNuiCallback("selected_character", (data: any, cb: any) => {
   const context = data.data;
   console.log(context, ' : NUI CALLBACK')
   emitNet("MUTINY:CORE:SERVER:SELECTED_CHARACTER", context);
+  cb('ok')
 });
 
-onNet("MUTINY:CORE:CLIENT:LOAD_CHARACTER", (context: { last_location: { x: any; y: any; z: any; heading: number; }; model: { model: any; }; }) => {
+onNet("MUTINY:CORE:CLIENT:LOAD_CHARACTER", (context: any) => {
+  console.log(context.model.model, ' THIS IS CONTEXT')
     exp.spawnmanager.setAutoSpawnCallback(() => {
       exp.spawnmanager.spawnPlayer(
         {
