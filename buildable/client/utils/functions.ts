2d2566906c11887247d94d56840cba02ccbf3a02
Yumshot <Clearlyio@protonmail.com>
2023-10-15 10:24:04 -0700

    feat(vehicle): functions for vehicle hud/systems

--- buildable/client/utils/functions.ts
+++ buildable/client/utils/functions.ts
@@ -1,1 +1,4 @@
 export const Delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+export const Speed = (vehicle: number) => Math.floor(GetEntitySpeed(vehicle) * 2.236936);
+export const Rpm = (vehicle: number) => Math.floor(GetVehicleCurrentRpm(vehicle) * 100);
+export const VehicleHealth = (vehicle: number) => GetVehicleEngineHealth(vehicle);
