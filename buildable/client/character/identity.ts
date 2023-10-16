aab65504b8b1fd1f58f5f2e2ab54c328b24c866a
Yumshot <Clearlyio@protonmail.com>
2023-10-15 19:40:15 -0700

    feat(debug): debuglocation command

--- buildable/client/character/identity.ts
+++ buildable/client/character/identity.ts
@@ -24,3 +24,19 @@ RegisterCommand(
   },
   false
 );
+
+const debugLocation = () => {
+  const ped = PlayerPedId();
+  const coords = GetEntityCoords(ped, true);
+  const heading = GetEntityHeading(ped);
+  const rotation = GetGameplayCamRot(2);
+  const output = `{x: ${coords[0]}, y: ${coords[1]}, z: ${coords[2]}, heading: ${heading}, rotation: ${rotation}}`;
+  emit("chat:addMessage", {
+    args: [output],
+  });
+
+  console.log(output);
+}
+
+
+RegisterCommand("loc", debugLocation, false)
