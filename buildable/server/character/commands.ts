b2f3613b81c947fe6bdb047ef84571c5166eaca5
Yumshot <Clearlyio@protonmail.com>
2023-10-16 10:26:11 -0700

    feat(commands): debug commands

---
+++ buildable/server/character/commands.ts
@@ -0,0 +1,97 @@
+RegisterCommand(
+  "tppos",
+  (source: any, args: any, raw: any) => {
+    emitNet("forfi-debugtools:tppos", source, args);
+  },
+  true
+);
+
+RegisterCommand(
+  "tpto",
+  (source: any, args: any[], raw: any) => {
+    emitNet("forfi-debugtools:tpto", source, args[0]);
+  },
+  true
+);
+
+RegisterCommand(
+  "tpm",
+  (source: any, args: any, raw: any) => {
+    emitNet("forfi-debugtools:tpwaypoint", source);
+  },
+  true
+);
+
+RegisterCommand(
+  "testsound",
+  (source: any, args: any, raw: any) => {
+    emitNet("forfi-debugtools:testsound", source, args);
+  },
+  false
+);
+
+RegisterCommand(
+  "getpos",
+  (source: any, args: any, raw: any) => {
+    emitNet("forfi-debugtools:getpos", source);
+  },
+  false
+);
+
+RegisterCommand(
+  "car",
+  (source: any, args: any[], raw: any) => {
+    if (args[0]) {
+      emitNet("forfi-debugtools:spawnVeh", source, args[0]);
+    }
+  },
+  true
+);
+
+RegisterCommand(
+  "getid",
+  (source: any, args: any, raw: any) => {
+    const player = source;
+    let steamId;
+    let licenseId;
+    for (let i = 0; i < GetNumPlayerIdentifiers(player); i++) {
+      let tempid = GetPlayerIdentifier(player, i);
+      console.log(tempid);
+      if (tempid.includes("steam")) {
+        steamId = tempid;
+      } else if (tempid.includes("license")) {
+        licenseId = tempid;
+      }
+    }
+    emitNet("forfi-debugtools:getid", source, steamId, licenseId);
+  },
+  false
+);
+
+RegisterCommand(
+  "debugcam",
+  (source: any, args: any, raw: any) => {
+    emitNet("forfi-debugtools:debugcam", source);
+  },
+  true
+);
+
+RegisterCommand(
+  "campos",
+  (source: any, args: any, raw: any) => {
+    emitNet("forfi-debugtools:campos", source);
+  },
+  true
+);
+
+RegisterCommand(
+  "setcampos",
+  (source: any, args: any, raw: any) => {
+    emitNet("forfi-debugtools:setcampos", source, args);
+  },
+  true
+);
+
+RegisterCommand("dv", (source: any, args: any, raw: any) => {
+  emitNet("MUTINY:CORE:CLIENT:COMMAND:REMOVE_VEHICLE", source);
+}, false);
