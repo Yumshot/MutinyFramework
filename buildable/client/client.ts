8cd448f9ad120ed7f5ae0d1c625caf6d8ee2540f
Yumshot <Clearlyio@protonmail.com>
2023-10-17 07:39:53 -0700

    refactor(import): remove import for new handler/controller

--- buildable/client/client.ts
+++ buildable/client/client.ts
@@ -2,7 +2,6 @@ import "./character/spawn";
 import "./handlers/callbacks";
 import "./handlers/teleport";
 // import './events/vehicle'
-import "./character/discord";
 import "./handlers/tick";
 import './character/identity';
 import "./handlers/commands";
