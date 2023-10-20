636f6cee76918d749a24b12102f56907dbe8d4bc
Yumshot <Clearlyio@protonmail.com>
2023-10-19 18:36:14 -0700

    chore(commands): identity check command, by character

--- buildable/client/character/identity.ts
+++ buildable/client/character/identity.ts
@@ -4,24 +4,33 @@ import { Delay } from "utils/functions";
 let characterIdentity: any = null;
 
 onNet("MUTINY:CORE:CLIENT:CHARACTER:IDENTITY", async (reference: any) => {
-    characterIdentity = reference;
-})
+  characterIdentity = reference;
+});
 
 RegisterCommand(
   "id",
   async (source: any, args: string | any[], raw: any) => {
-    console.log("MUTINY>CORE>CLIENT>CHARACTER>IDENTITY>COMMAND (source) ", source);
+    console.log(
+      "MUTINY>CORE>CLIENT>CHARACTER>IDENTITY>COMMAND (source) ",
+      source
+    );
     emitNet(
       "MUTINY:CORE:SERVER:DATABASE:EVENTS:GATHER_USER_DATA_FOR_CLIENT",
       "MUTINY:CORE:CLIENT:CHARACTER:IDENTITY"
     );
+
     await Delay(1000);
-    console.log("MUTINY>CORE>CLIENT>CHARACTER>IDENTITY>COMMAND (message) ", characterIdentity.characters[characterIdentity.last_character]);
-      emit("chat:addMessage", {
-        args: [characterIdentity.characters[characterIdentity.last_character]],
-      });
+    const target =
+      characterIdentity.characters[characterIdentity.last_character];
+
+    console.log(
+      "MUTINY>CORE>CLIENT>CHARACTER>IDENTITY>COMMAND (message) ",
+      target.first_name,
+      target.last_name,
+      target.index
+    );
   },
   false
 );
 
-RegisterCommand("loc", DebugLocation, false)
+RegisterCommand("loc", DebugLocation, false);
