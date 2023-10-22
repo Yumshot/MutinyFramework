7a0a316c51ec7ad36a0d11d07ccb1c97040db3ed
Yumshot <Clearlyio@protonmail.com>
2023-10-22 16:33:33 -0700

    feat(blips): setup default blips

--- buildable/client/handlers/blips.ts
+++ buildable/client/handlers/blips.ts
@@ -1,10 +1,8 @@
-import { Delay } from "utils/functions";
 import {
   BARBERSHOP_BLIPS,
   CLOTHING_STORE_BLIPS,
   GAS_BLIPS,
 } from "../../common/blips";
-import { IBlip } from "interfaces/Blip";
 
 const DEFAULT_BLIPS = [
   ...BARBERSHOP_BLIPS,
@@ -12,26 +10,18 @@ const DEFAULT_BLIPS = [
   ...GAS_BLIPS,
 ];
 
-const MAX_BLIP_DISTANCE = 75; // Change this to the maximum distance
-
-// Create blips for all of the default blip locations.
-export const CreateBlips = async (): Promise<void> => {
-  for (let index = 0; index < DEFAULT_BLIPS.length; index++) {
-    const element = DEFAULT_BLIPS[index];
-    if (!element.hidden && !element.hasOwnProperty("id")) {
-      const blip = AddBlipForCoord(element.x, element.y, element.z);
-      SetBlipSprite(blip, element.blip);
-      SetBlipDisplay(blip, 4);
-      SetBlipScale(blip, element.scale);
-      SetBlipColour(blip, element.colour);
-      SetBlipAsShortRange(blip, true);
-      BeginTextCommandSetBlipName("STRING");
-      AddTextComponentString(element.name);
-      EndTextCommandSetBlipName(blip);
-    } else {
-      console.log("Blip is hidden or already has an ID.");
-    }
+const CreateDefaultBlips = () => {
+  for (const blip of DEFAULT_BLIPS) {
+    blip.handle = AddBlipForCoord(blip.x, blip.y, blip.z);
+    SetBlipSprite(blip.handle, blip.blip);
+    SetBlipDisplay(blip.handle, 4);
+    SetBlipScale(blip.handle, blip.scale);
+    SetBlipColour(blip.handle, blip.colour);
+    SetBlipAsShortRange(blip.handle, true);
+    BeginTextCommandSetBlipName("STRING");
+    AddTextComponentString(blip.name);
+    EndTextCommandSetBlipName(blip.handle);
   }
 };
 
-CreateBlips();
+CreateDefaultBlips();
