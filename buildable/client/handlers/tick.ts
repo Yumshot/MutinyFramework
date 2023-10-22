676208bbd61da322209375576f02da79635f2712
Yumshot <Clearlyio@protonmail.com>
2023-10-22 16:34:11 -0700

    feat(veh hud): started display

--- buildable/client/handlers/tick.ts
+++ buildable/client/handlers/tick.ts
@@ -1,17 +1,76 @@
-import { Delay, GtaHudRadar } from "utils/functions";
+import { Delay } from "utils/functions";
 import { HasPlayerStatusChanged, PLAYER_STATUS } from "./character";
-import { BAN_WEAPONS, DISCORD } from "../../common/globals";
-import { DiscordRichPresence } from "handlers/discord";
 
 const minimap = RequestScaleformMovie("minimap");
 SetMaxWantedLevel(0);
-
+let isVehHudActive = false;
+let timer = 1000;
 setTick(async () => {
   if (HasPlayerStatusChanged()) {
     DisplayRadar(PLAYER_STATUS.isInVehicle);
-    // emitNet("MUTINY:CORE:SERVER:PLAYER_STATUS_UPDATE", playerStatus);
   }
+
   SetVehicleDensityMultiplierThisFrame(0.1);
+
+  if (PLAYER_STATUS.isInVehicle && !IsPauseMenuActive()) {
+    const ped = PlayerPedId();
+    const veh = GetVehiclePedIsIn(ped, true);
+    if (!veh) return;
+    const vehClass = GetVehicleClass(veh);
+    const vehModel = GetEntityModel(veh);
+    const vehName = GetDisplayNameFromVehicleModel(vehModel);
+    const vehPlate = GetVehicleNumberPlateText(veh);
+    const vehSpeed = GetEntitySpeed(veh);
+    const vehSpeedKmh = Math.round(vehSpeed * 3.6);
+    const vehSpeedMph = Math.round(vehSpeed * 2.236936);
+    const vehSpeedKnots = Math.round(vehSpeed * 1.943844);
+    const vehFuel = GetVehicleFuelLevel(veh);
+    const vehMaxFuel = GetVehicleHandlingFloat(
+      veh,
+      "CHandlingData",
+      "fPetrolTankVolume"
+    );
+    const vehFuelPercentage = Math.round((vehFuel / vehMaxFuel) * 100);
+    const vehHealth = GetVehicleEngineHealth(veh);
+    const vehHealthPercentage = Math.round((vehHealth / 1000) * 100);
+    const vehBodyHealth = GetVehicleBodyHealth(veh);
+    const vehBodyHealthPercentage = Math.round((vehBodyHealth / 1000) * 100);
+
+    SendNUIMessage({
+      event: "VEHICLE_HUD",
+      state: true,
+      options: {
+        speed: {
+          kmh: vehSpeedKmh,
+          mph: vehSpeedMph,
+          knots: vehSpeedKnots,
+        },
+        fuel: {
+          level: vehFuel,
+          percentage: vehFuelPercentage,
+        },
+        health: {
+          engine: {
+            level: vehHealth,
+            percentage: vehHealthPercentage,
+          },
+          body: {
+            level: vehBodyHealth,
+            percentage: vehBodyHealthPercentage,
+          },
+        },
+      },
+    });
+    isVehHudActive = true;
+    timer = 250;
+  } else {
+    SendNUIMessage({
+      event: "VEHICLE_HUD",
+      state: false,
+    });
+    timer = 1000;
+    isVehHudActive = false;
+  }
 
   // Update minimap health and armor
   if (minimap !== 0) {
@@ -20,5 +79,5 @@ setTick(async () => {
     EndScaleformMovieMethod();
   }
 
-  await Delay(1000);
+  await Delay(timer);
 });
