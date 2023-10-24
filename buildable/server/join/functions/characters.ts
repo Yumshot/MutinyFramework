7bb918e1da7dc167384993cc5f7bc35f0dffa294
Yumshot <Clearlyio@protonmail.com>
2023-10-23 20:52:23 -0700

    nothing

--- buildable/server/join/functions/characters.ts
+++ buildable/server/join/functions/characters.ts
@@ -1,9 +1,8 @@
 import { GATHER_USER_DATA_FOR_SERVER } from "database/functions";
 
-on("playerJoining", async (source: any, oldID: any) => {
+on("playerJoining", async (source: any, _oldID: any) => {
     const src = source;
     const GATHERED_USER = await GATHER_USER_DATA_FOR_SERVER(src);
-    console.log("MUTINY>SERVER>JOIN>FUNCTIONS>CHARACTERS playerJoining (GATHERED_USER) ", GATHERED_USER);
     emitNet("MUTINY:CORE:CLIENT:SPAWN:SET_DATA", src, GATHERED_USER);
 });
 
