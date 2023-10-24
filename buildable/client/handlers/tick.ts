825070a3675fe39568beb803ecb800ff0713512d
Yumshot <Clearlyio@protonmail.com>
2023-10-24 13:50:41 -0700

    feat(worldsync/objects): Sets / and Timed objects

--- buildable/client/handlers/tick.ts
+++ buildable/client/handlers/tick.ts
@@ -2,24 +2,31 @@ import { Delay } from "utils/functions";
 import { HasPlayerStatusChanged, PLAYER_STATUS } from "./character";
 
 const minimap = RequestScaleformMovie("minimap");
+
 SetMaxWantedLevel(0);
+SetAggressiveHorns(false);
+SetPoliceIgnorePlayer(PlayerId(), true);
+SetPoliceRadarBlips(false);
+SetCreateRandomCops(false);
+SetCreateRandomCopsNotOnScenarios(false);
+SetGarbageTrucks(false);
+SwitchTrainTrack(0, true);
+SetTrainTrackSpawnFrequency(0, 5);
+SetRandomTrains(true);
+SetRandomBoats(false);
+
 let isVehHudActive = false;
 let timer = 1000;
+
 setTick(async () => {
   if (HasPlayerStatusChanged()) {
     DisplayRadar(PLAYER_STATUS.isInVehicle);
   }
 
-  SetVehicleDensityMultiplierThisFrame(0.1);
-
   if (PLAYER_STATUS.isInVehicle && !IsPauseMenuActive()) {
     const ped = PlayerPedId();
     const veh = GetVehiclePedIsIn(ped, true);
     if (!veh) return;
-    const vehClass = GetVehicleClass(veh);
-    const vehModel = GetEntityModel(veh);
-    const vehName = GetDisplayNameFromVehicleModel(vehModel);
-    const vehPlate = GetVehicleNumberPlateText(veh);
     const vehSpeed = GetEntitySpeed(veh);
     const vehSpeedKmh = Math.round(vehSpeed * 3.6);
     const vehSpeedMph = Math.round(vehSpeed * 2.236936);
@@ -80,4 +87,50 @@ setTick(async () => {
   }
 
   await Delay(timer);
+});
+
+const { exports: exp } = global as any;
+
+exp["cfx-anes-worldsync"].useTime({
+  range: [7, 19],
+  onStateChange: (state: any) => {
+    if (state) {
+      const objectPool = GetGamePool("CObject");
+      for (let i = 0; i < objectPool.length; i++) {
+        const hash = GetEntityModel(objectPool[i]);
+        if (hash === -1852609017) {
+          const [x, y, z] = GetEntityCoords(objectPool[i], false);
+          const checkSpot = [901.157, -1685.904, 46.384];
+          const xCheck = Math.round(x) === Math.round(checkSpot[0]);
+          const yCheck = Math.round(y) === Math.round(checkSpot[1]);
+          const zCheck = Math.round(z) === Math.round(checkSpot[2]);
+          if (xCheck && yCheck && zCheck) {
+            SetEntityHeading(objectPool[i], -94.519);
+          }
+        }
+      }
+    }
+  },
+});
+
+exp["cfx-anes-worldsync"].useTime({
+  range: [19, 7],
+  onStateChange: (state: any) => {
+    if (state) {
+      const objectPool = GetGamePool("CObject");
+      for (let i = 0; i < objectPool.length; i++) {
+        const hash = GetEntityModel(objectPool[i]);
+        if (hash === -1852609017) {
+          const [x, y, z] = GetEntityCoords(objectPool[i], false);
+          const checkSpot = [901.157, -1685.904, 46.384];
+          const xCheck = Math.round(x) === Math.round(checkSpot[0]);
+          const yCheck = Math.round(y) === Math.round(checkSpot[1]);
+          const zCheck = Math.round(z) === Math.round(checkSpot[2]);
+          if (xCheck && yCheck && zCheck) {
+            SetEntityHeading(objectPool[i], -174.219);
+          }
+        }
+      }
+    }
+  },
 });
