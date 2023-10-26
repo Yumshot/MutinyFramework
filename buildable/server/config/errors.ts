aacf07d5fe0782c747f49fba64e5eacf4c7052c2
Yumshot <Clearlyio@protonmail.com>
2023-10-25 21:30:45 -0700

    feat(errors): add error messages

--- buildable/server/config/errors.ts
+++ buildable/server/config/errors.ts
@@ -1,11 +1,13 @@
 export const ErrorKeys = {
   0: TextToASCII("CREATEUSER(__EXECUTE()): FAILED"),
+  1: TextToASCII("UPDATEUSER(): FAILED"),
 };
 
 /**
  * Converts a given string to ASCII code.
  * @param text - The string to convert to ASCII code.
  * @returns The ASCII code of the given string.
+ * LINK - https://codebeautify.org/ascii-to-text
  */
 function TextToASCII(text: string) {
   let ascii = "";
