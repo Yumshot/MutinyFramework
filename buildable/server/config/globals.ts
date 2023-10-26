8f48da224dbe1adf9c1bc17acf48583f74e223de
Yumshot <Clearlyio@protonmail.com>
2023-10-25 21:31:33 -0700

    config(globals): server sided globals

--- buildable/server/config/globals.ts
+++ buildable/server/config/globals.ts
@@ -14,6 +14,16 @@ export const __locations = {
 };
 =======
 export const __databaseLocales = {
+<<<<<<< HEAD
     connection: 'mongodb://127.0.0.1:27017/mutiny_roleplay',
 }
 >>>>>>> 9b889d0 (Massive update to the server, this is a complete rewrite of the server.)
+=======
+  connection: "mongodb://127.0.0.1:27017/mutiny_roleplay",
+};
+
+export const __routingBuckets = {
+  world: 1,
+  login: 69,
+};
+>>>>>>> 3e66796 (config(globals): server sided globals)
