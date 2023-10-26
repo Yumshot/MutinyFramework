6ef2d296487689ed2de2574e53d39b34e77d3b1e
Yumshot <Clearlyio@protonmail.com>
2023-10-25 21:33:40 -0700

    chore(server): controllers index

--- buildable/server/controllers/index.ts
+++ buildable/server/controllers/index.ts
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import "./playerConnecting";
 import "./playerJoining";
 import "./onResourceStart";
@@ -8,3 +9,10 @@ import "./onResourceStart";
 =======
 import './playerConnecting'
 >>>>>>> 9b889d0 (Massive update to the server, this is a complete rewrite of the server.)
+=======
+import "./playerConnecting";
+import "./playerJoining";
+import "./entityCreating";
+import "./entityCreated";
+import "./mumbleConnected";
+>>>>>>> d9eecdc (chore(server): controllers index)
