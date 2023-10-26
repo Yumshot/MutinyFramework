8d0114ebfff88915a04346a5980c99d777d53ed7
Yumshot <Clearlyio@protonmail.com>
2023-10-25 21:30:45 -0700

    feat(errors): add error messages

--- buildable/server/config/errors.ts
+++ buildable/server/config/errors.ts
@@ -1,10 +1,14 @@
 export const ErrorKeys = {
   0: TextToASCII("CREATEUSER(__EXECUTE()): FAILED"),
 <<<<<<< HEAD
+<<<<<<< HEAD
   1: TextToASCII("UPDATEUSER(): FAILED"),
   2: TextToASCII("SETNEWCHARACTERDATA(): FAILED"),
 =======
 >>>>>>> 9b889d0 (Massive update to the server, this is a complete rewrite of the server.)
+=======
+  1: TextToASCII("UPDATEUSER(): FAILED"),
+>>>>>>> aacf07d (feat(errors): add error messages)
 };
 
 /**
@@ -12,9 +16,13 @@ export const ErrorKeys = {
  * @param text - The string to convert to ASCII code.
  * @returns The ASCII code of the given string.
 <<<<<<< HEAD
+<<<<<<< HEAD
  * LINK - https://codebeautify.org/ascii-to-text
 =======
 >>>>>>> 9b889d0 (Massive update to the server, this is a complete rewrite of the server.)
+=======
+ * LINK - https://codebeautify.org/ascii-to-text
+>>>>>>> aacf07d (feat(errors): add error messages)
  */
 function TextToASCII(text: string) {
   let ascii = "";
