908bf2ce1a3f2a06a3304eb81ccfa2482f6c4476
Yumshot <Clearlyio@protonmail.com>
2023-10-26 21:15:44 -0700

    chore(ui): add ui / might not use this.

--- buildable/client/controllers/onGameEventTriggered.ts
+++ buildable/client/controllers/onGameEventTriggered.ts
@@ -13,6 +13,20 @@ on("gameEventTriggered", (name: any, args: any[]) => {
   if (name === "CEventNetworkStartMatch") {
     if (__playerCharacters.length === 0) {
       // TODO: Create a new character.
+      const DEFAULT_SPAWN = { x: 7614.787, y: 1064.8, z: 1678.407 };
+      exp.spawnmanager.setAutoSpawnCallback(() => {
+        exp.spawnmanager.spawnPlayer(
+          {
+            x: DEFAULT_SPAWN.x,
+            y: DEFAULT_SPAWN.y,
+            z: DEFAULT_SPAWN.z,
+            model: "a_m_m_skater_01",
+          },
+          () => {}
+        );
+      });
+      exp.spawnmanager.setAutoSpawn(true);
+      exp.spawnmanager.forceRespawn();
     } else {
       // TODO: Send Character Select Screen. --> into Spawn Character.
     }
