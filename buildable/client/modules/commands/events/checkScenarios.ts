9e96a01acf097085f4c877c2b667e4c7de16562f
Yumshot <Clearlyio@protonmail.com>
2023-10-30 16:05:33 -0700

    Add event to check and handle scenarios
    This commit adds a new file called `checkScenarios.ts` which contains code to check and handle different scenarios. It imports the `__scenarios` variable from the `config/enums/scenarios` module and the `Delay` module from `modules/utils/delay`.
    
    The `onNet("checkScenarios")` event is triggered when the scenarios need to be checked. It sets up an interval that iterates through the scenarios and emits the `handleScenario` event with the current scenario.
    
    The `on("handleScenario")` event handles the received scenario. If the scenario is "Standing", it clears the player's tasks immediately. Otherwise, it starts the specified scenario in place.
    
    Finally, the `onNet("clearScenario")` event clears the current scenario immediately.

--- buildable/client/modules/commands/events/checkScenarios.ts
+++ buildable/client/modules/commands/events/checkScenarios.ts
@@ -1,4 +1,5 @@
 import { __scenarios } from "config/enums/scenarios";
+<<<<<<< HEAD
 let tick = 0;
 
 /**
@@ -6,17 +7,30 @@ let tick = 0;
  */
 onNet("checkScenarios", async () => {
   const scenarios = __scenarios;
+=======
+import { Delay } from "modules/utils/delay";
+
+let tick = 0;
+onNet("checkScenarios", async () => {
+  const scenarios = __scenarios;
+  console.log("DOES THIS WORK?");
+>>>>>>> f5f09b9 (Add event to check and handle scenarios)
   setInterval(() => {
     tick++;
     emit("handleScenario", scenarios[tick]);
   }, 10000);
 });
 
+<<<<<<< HEAD
 /**
  * Listens for the "handleScenario" event and handles the given scenario for the player character.
  * @param scenario - The scenario to handle for the player character.
  */
 on("handleScenario", (scenario: string) => {
+=======
+on("handleScenario", (scenario: string) => {
+  console.log("DOES THIS WORK? " + scenario);
+>>>>>>> f5f09b9 (Add event to check and handle scenarios)
   const playerPed = PlayerPedId();
   if (scenario === "Standing") {
     ClearPedTasksImmediately(playerPed);
@@ -25,9 +39,12 @@ on("handleScenario", (scenario: string) => {
   }
 });
 
+<<<<<<< HEAD
 /**
  * Listens for the "clearScenario" event and clears the current scenario for the player character.
  */
+=======
+>>>>>>> f5f09b9 (Add event to check and handle scenarios)
 onNet("clearScenario", () => {
   ClearPedTasksImmediately(PlayerPedId());
 });
