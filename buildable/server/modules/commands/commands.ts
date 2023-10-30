7d4cafd997b233bba63a6c99723b5027a40ddebe
Yumshot <Clearlyio@protonmail.com>
2023-10-30 16:07:43 -0700

    feat: Add commandHandler module
    This commit adds the commandHandler module, which registers and handles commands in the command system. The module imports the commands object from the commands module and registers each command using the `RegisterCommand` function. It also creates a list of key-value pairs containing the command's key, description, and usage. Finally, it emits the command suggestions to the chat using the `emitNet` function.
    
    - Adds the `commandHandler.ts` file
    - Imports the `__commandSystem` object
    - Registers each command using `RegisterCommand`
    - Creates a list of key-value pairs containing the command's key, description, and usage
    - Emits the command suggestions to the chat using `emitNet`

--- buildable/server/modules/commands/commands.ts
+++ buildable/server/modules/commands/commands.ts
@@ -1,12 +1,18 @@
+<<<<<<< HEAD
 import { CreateWorldVehicle } from "./events/requestVehicle";
+=======
+>>>>>>> a59d569 (feat: Add commandHandler module)
 import { getUserHex } from "./functions/getUserHex";
 
 /**
  * Object containing various commands for the command system.
  */
+<<<<<<< HEAD
 /**
  * Defines a collection of commands for the Mutiny Framework server.
  */
+=======
+>>>>>>> a59d569 (feat: Add commandHandler module)
 export const __commandSystem = {
   /**
    * Teleports the player to the marker on the map.
@@ -25,13 +31,18 @@ export const __commandSystem = {
    * @param model - The model of the vehicle to spawn.
    */
   car: {
+<<<<<<< HEAD
     event: (source: any) => CreateWorldVehicle(source[0], source[1]),
+=======
+    event: (source: any) => emitNet("spawnVehicle", source[0], source[1]),
+>>>>>>> a59d569 (feat: Add commandHandler module)
     async: true,
     restricted: true,
     description: "Spawns a vehicle",
     usage: "/car [model]",
   },
   /**
+<<<<<<< HEAD
    * Removes vehicle for the player.
    * @param source - The player to spawn the vehicle for.
    */
@@ -43,6 +54,8 @@ export const __commandSystem = {
     usage: "/dv",
   },
   /**
+=======
+>>>>>>> a59d569 (feat: Add commandHandler module)
    * Gets the hex of a player.
    * @param source - The player to get the hex of.
    */
@@ -59,10 +72,13 @@ export const __commandSystem = {
     description: "Gets the hex of a player",
     usage: "/hex [player] or /hex for self",
   },
+<<<<<<< HEAD
   /**
    * Spawns a random scenario for the player.
    * @param source - The player to spawn the scenario for.
    */
+=======
+>>>>>>> a59d569 (feat: Add commandHandler module)
   scenario: {
     event: (source: any) => emitNet("checkScenarios", source[0]),
     async: true,
@@ -70,10 +86,13 @@ export const __commandSystem = {
     description: "Spawns a random scenario",
     usage: "/scenario",
   },
+<<<<<<< HEAD
   /**
    * Clears the scenario for the player.
    * @param source - The player to clear the scenario for.
    */
+=======
+>>>>>>> a59d569 (feat: Add commandHandler module)
   clearScenario: {
     event: (source: any) => emitNet("clearScenario", source[0]),
     async: true,
@@ -81,10 +100,13 @@ export const __commandSystem = {
     description: "Clears the scenario",
     usage: "/clearScenario",
   },
+<<<<<<< HEAD
   /**
    * Gets the routing bucket of a player.
    * @param source - The player to get the routing bucket of.
    */
+=======
+>>>>>>> a59d569 (feat: Add commandHandler module)
   getRoutingBucket: {
     event: (source: any) => {
       console.log(GetPlayerRoutingBucket(source[0]));
@@ -94,11 +116,14 @@ export const __commandSystem = {
     description: "Gets the routing bucket of a player",
     usage: "/getRoutingBucket [player]",
   },
+<<<<<<< HEAD
   /**
    * Sets the routing bucket of a player.
    * @param source - The player to set the routing bucket for.
    * @param bucket - The routing bucket to set.
    */
+=======
+>>>>>>> a59d569 (feat: Add commandHandler module)
   setRoutingBucket: {
     event: (source: any) => {
       SetPlayerRoutingBucket(source[0].toString(), source[1]);
@@ -108,6 +133,7 @@ export const __commandSystem = {
     description: "Sets the routing bucket of a player",
     usage: "/setRoutingBucket [player] [bucket]",
   },
+<<<<<<< HEAD
   respawn: {
     event: (source: any) => {
       emitNet("respawnPlayer", source[0]);
@@ -117,4 +143,6 @@ export const __commandSystem = {
     description: "Respawns the player",
     usage: "/respawn",
   },
+=======
+>>>>>>> a59d569 (feat: Add commandHandler module)
 };
