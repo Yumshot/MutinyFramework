88319a29400e0fe7fa6dc3b51a137a8aaf2aba1d
Yumshot <Clearlyio@protonmail.com>
2023-10-30 16:04:46 -0700

    feat(add Vector3)

--- buildable/server/classes/Vector3.ts
+++ buildable/server/classes/Vector3.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * Creates a new Vector3 instance.
  * @param coords - The x, y, and z coordinates of the vector.
@@ -19,6 +20,12 @@ export class Vector3 {
    */
   z: number;
 
+=======
+export class Vector3 {
+  x: number;
+  y: number;
+  z: number;
+>>>>>>> b9d8dcb (feat(add Vector3))
   constructor(coords: [number, number, number]) {
     this.x = coords[0];
     this.y = coords[1];
