3980023773609a78e5819400e384546ebac30d9b
Yumshot <Clearlyio@protonmail.com>
2023-10-30 22:21:21 -0700

    Add deleteVehicle, respawnPlayer, and spawnVehicle events handling.

--- buildable/client/modules/commands/index.ts
+++ buildable/client/modules/commands/index.ts
@@ -1,8 +1,14 @@
 import "./events/teleportToMarker";
 import "./events/checkScenarios";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import "./events/spawnVehicle";
 import "./events/deleteVehicle";
 import "./events/respawnPlayer";
 =======
 >>>>>>> 37db202 (chore(imports/cleanup))
+=======
+import "./events/spawnVehicle";
+import "./events/deleteVehicle";
+import "./events/respawnPlayer";
+>>>>>>> ff9dd14 (Add deleteVehicle, respawnPlayer, and spawnVehicle events handling.)
