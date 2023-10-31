be43b9cca13644aef94d5e20343575ee0c205b11
Yumshot <Clearlyio@protonmail.com>
2023-10-30 22:38:34 -0700

    removal of all old files

--- buildable/old/common/models.ts
+++
@@ -1,6 +0,0 @@
-export const ATM_MODELS = [
-  "prop_atm_01",
-  "prop_atm_02",
-  "prop_atm_03",
-  "prop_fleeca_atm",
-];
--- buildable/old/old/Bank.ts
+++
@@ -1,9 +0,0 @@
-// Define the interface for a bank account object
-export interface BankAccount {
-  id: number;
-  name: string;
-  acc_number: string;
-  balance: number;
-  transactions: object[];
-  loans: object[];
-}
