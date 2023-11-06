e70f3a7f2801a97f4c5bafa8b9bd6732d8ad737b
Yumshot <Clearlyio@protonmail.com>
2023-11-05 16:20:37 -0800

    Refactor character build code with new default values
    The code changes refactor the character build code to use new default values for various properties. This ensures that characters have appropriate default values when created. The changes include setting default values for cash, bank, pin, crypto, appearance, weapons, vehicles, pilot licenses, businesses, income level, personal history, needs, medical status, and medical history. The code also updates the method used to build the character.

--- buildable/server/modules/characters/build.ts
+++ buildable/server/modules/characters/build.ts
@@ -4,17 +4,80 @@ import { __databaseInstance } from "server";
 
 onNet("CreateNewUserCharacter", async (data: any) => {
   const steam_target = FindSteam(source);
-  const character = new CreateNewCharacter(
-    steam_target, // get index from DB
+  const base_license = {
+    id: 0,
+    active: false,
+    owned: false,
+  };
+
+  const newCharacter = new CreateNewCharacter(
+    steam_target,
     data.first_name,
     data.last_name,
     data.sex,
     data.age,
     data.internet_handle,
     { x: 0, y: 0, z: 0, heading: 0 },
-    { cash: 0, bank: [], pin: data.pin }
+    {
+      cash: 500,
+      bank: new Array(),
+      pin: data.pin,
+      crypto: {
+        yumSats: 0,
+        jizzCoin: 0,
+        transactions: new Array(),
+      },
+    },
+    data.appearance,
+    {
+      weapon: {
+        possession: base_license,
+        hunting: base_license,
+        sportShooting: base_license,
+        dealers: base_license,
+        classIII: base_license,
+      },
+      vehicle: {
+        classD: base_license,
+        classC: base_license,
+        classA: base_license,
+        classM: base_license,
+      },
+      pilot: {
+        commercial: base_license,
+        private: base_license,
+        airTrafficController: base_license,
+      },
+      business: {
+        business: new Array(),
+        realEstate: base_license,
+        stockBroker: base_license,
+      },
+    },
+    "low-income",
+    {
+      warrants: new Array(),
+      convictions: new Array(),
+      arrests: new Array(),
+      tickets: new Array(),
+    },
+    new Array(),
+    new Array(),
+    {
+      needs: {
+        health: 100,
+        hunger: 100,
+        thirst: 100,
+        bloodLevel: 100,
+        oxygenLevel: 100,
+        stress: 0,
+      },
+      medicalStatus: "healthy",
+      medicalHistory: new Array(),
+      medicalBills: new Array(),
+    }
   );
-  const __character = await character.build();
+  const __character = await newCharacter.build();
   try {
     await __databaseInstance.InsertNewCharacterToUserCharacterData({
       steam_target: steam_target,
